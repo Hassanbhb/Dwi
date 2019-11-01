3745b69ca5129b25f0a2e06596d672cd0b7fadae
Hassanbhb <bussinesbhb@gmail.com>
2019-11-01 10:45:13 +0100

    added validation to profile

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../../models/user");
 const bcrypt = require("bcrypt");
+const { check, validationResult } = require("express-validator");
 const { ensureAuthenticated } = require("../../config/auth");
 
 router.get("/profile", ensureAuthenticated, (req, res) => {
@@ -15,53 +16,85 @@ router.get("/profile", ensureAuthenticated, (req, res) => {
 
 //TODO: split to two, so that we can update posts
 //the user wrote
-router.put("/profile", ensureAuthenticated, (req, res) => {
-  // check if password and confirm password exist
-  if (req.body.newPassword && req.body.confirmPassword) {
-    //check if they are equal
-    if (req.body.newPassword === req.body.confirmPassword) {
-      //hash the new password
-      bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
-        if (err) {
-          console.log("profilejs: ", err);
-        }
+router.put(
+  "/profile",
+  ensureAuthenticated,
+  //validating user input
+  [
+    check("firstName", "Name must be Alphanumeric")
+      .trim()
+      .escape()
+      .isLength({ min: 2 }),
+    check("lastName", "last name must be Alphanumeric")
+      .trim()
+      .escape()
+      .isLength({ min: 2 }),
+    check("email", "Please enter a valid email")
+      .not()
+      .isEmpty()
+      .isEmail(),
+    check("newPassword", "Password must have 5+ chars and at least one number")
+      .not()
+      .isEmpty()
+      .isLength({ min: 5 })
+      .matches(/\d/)
+      .optional({ checkFalsy: true }),
+    check("confirmPassword", "Passwords don't match")
+      .custom((value, { req }) => value === req.body.newPassword)
+      .optional({ checkFalsy: true })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    //if validation found errors then display them to user
+    if (!errors.isEmpty()) {
+      req.flash("error", `${errors.array()[0].msg}`);
+      res.redirect("/profile");
+    } else {
+      // check if password and confirm password exist
+      if (req.body.newPassword && req.body.confirmPassword) {
+        //hash the new password
+        bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+          if (err) {
+            console.log("profilejs: ", err);
+          }
+          const updated = {
+            name: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hash
+          };
+          Users.findByIdAndUpdate({ _id: req.user._id }, updated, {
+            useFindAndModify: false
+          })
+            .then(data => {
+              req.flash("success", "Profile Updated successfully");
+              res.redirect("/profile");
+            })
+            .catch(err => {
+              console.log("Error: ", err);
+            });
+        });
+      } else {
+        //if new password don't exist ten edit the rest
         const updated = {
           name: req.body.firstName,
           lastName: req.body.lastName,
-          email: req.body.email,
-          password: hash
+          email: req.body.email
         };
+        //update this users data
         Users.findByIdAndUpdate({ _id: req.user._id }, updated, {
           useFindAndModify: false
         })
           .then(data => {
+            req.flash("success", "Profile Updated successfully");
             res.redirect("/profile");
           })
           .catch(err => {
-            console.log("Error: ", err);
+            console.log(err);
           });
-      });
-    } else {
-      res.send("Error: Passwords don't match");
+      }
     }
-  } else {
-    //if new password don't exist ten edit the rest
-    const updated = {
-      name: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email
-    };
-    //update this users data
-    Users.findByIdAndUpdate({ _id: req.user._id }, updated, {
-      useFindAndModify: false
-    })
-      .then(data => {
-        res.redirect("/profile");
-      })
-      .catch(err => {
-        console.log(err);
-      });
   }
-});
+);
 
 module.exports = router;
