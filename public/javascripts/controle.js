74afcebf891cb7ee5d411897425a86b6dc24eb97
Hassanbhb <bussinesbhb@gmail.com>
2020-03-11 19:46:39 +0100

    added show btn functionalty in contol page

--- public/javascripts/controle.js
+++ public/javascripts/controle.js
@@ -15,7 +15,7 @@ delete_btns.forEach(btn => {
   btn.addEventListener("click", e => {
     let url = window.location.origin + "/controle/delete";
 
-    let Id = e.target.getAttribute("data-postId");
+    let Id = e.target.parentNode.getAttribute("data-postId");
     let method = "DELETE";
 
     const xhttp = new XMLHttpRequest();
@@ -29,5 +29,16 @@ delete_btns.forEach(btn => {
     };
 
     xhttp.send(`postId=${Id}`);
+  });
+});
+
+// Post content toggle
+
+const showBtns = document.querySelectorAll(".actions > div > img");
+showBtns.forEach(showBtn => {
+  showBtn.addEventListener("click", e => {
+    e.target.parentElement.parentElement.parentElement.nextSibling.classList.toggle(
+      "hide"
+    );
   });
 });
